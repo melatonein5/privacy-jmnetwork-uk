bcf2ba4f6060abd3780b4392ff5d20d62390d6e6
Joseph McCallum-Nattrass <jmn.jmnetwork.uk>
2024-06-17 19:40:27 +0100

    Added code

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+// ServeHandler function to serve the policy.html file
+func ServeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "policy.html")
+}
+
+func main() {
+	//Code to serve the policy.html file
+	http.HandleFunc("/", ServeHandler)
+	http.ListenAndServe(":8080", nil)
+}
